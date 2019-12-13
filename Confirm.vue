be54905a1dc63bfe73218de8054a0537d66c7536
tenghaiyang <tegraweb@163.com>
2019-12-13 11:29:17 +0800

    update loading,conform,nodata components

---
+++ Confirm.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <div class="anx-confirm-bgs" v-if="showValue">
+        </div>
+        <div :style="{width: confirmWidth + 'px'}" class="anx-confirm-boxs" v-if="showValue">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AnxConfirm',
+    data () {
+        return {
+            showValue: false
+        }
+    },
+    props: {
+        value: {
+            type: Boolean,
+            default: false
+        },
+        confirmWidth: {
+            type: Number,
+            default: 0
+        }
+    },
+    watch: {
+        value (val) {
+            this.showValue = val
+            if (val === true) {
+                document.documentElement.style.overflow = 'hidden'
+                document.body.style.overflow = 'hidden'
+            } else {
+                document.documentElement.style.overflow = ''
+                document.body.style.overflow = ''
+            }
+        }
+    },
+    methods: {
+    },
+    created () {
+        this.showValue = this.value
+        if (this.value) {
+            document.documentElement.style.overflow = 'hidden'
+            document.body.style.overflow = 'hidden'
+            this.showValue = this.value
+        }
+    }
+}
+</script>
+
+<style scoped lang="stylus" type="text/stylus">
+.anx-confirm-bgs {
+  position: fixed;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 9999;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background: #000;
+  opacity: 0.5;
+}
+.anx-confirm-boxs {
+  position: fixed;
+  left:50%;
+  top:45%;
+  transform:translateX(-50%) translateY(-50%);
+  padding: 30px 36px 0 24px;
+  background: #fff;
+  border-radius: 9px;
+  z-index: 99999;
+  color: #666;
+  font-size: 14px;
+  text-align: center;
+}
+</style>
---
+++ NoData.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="viscera-default">
+        <div class="txt">{{ noData ? noData : '有效数据不足！'}}</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'NoData',
+    data () {
+        return {
+        }
+    },
+    props: {
+        noData: String
+    },
+    mounted () {
+    },
+    created () {
+        document.body.style.background = '#fff'
+    },
+    methods: {
+    }
+}
+</script>
+
+<style scoped>
+    .viscera-default {
+        position: absolute;
+        margin-top: 200px;
+        font-size: 26px;
+        color: #898989;
+        text-align: center;
+        padding-bottom: 100px;
+    }
+    .viscera-default img {
+        width: 80%;
+    }
+    .txt {
+        position: absolute;
+        top: 460px;
+        left: 0;
+        right: 0;
+        margin: auto;
+    }
+</style>
